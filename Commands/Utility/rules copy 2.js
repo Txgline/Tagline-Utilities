9e3904bdaf239fbd7c3c3915c5f7a8367c1fc2a9
txg <146649145+Txgline@users.noreply.github.com>
2025-09-29 19:17:48 +0300

    uytt

--- Commands/Utility/rules copy 2.js
+++ Commands/Utility/rules copy 2.js
@@ -9,12 +9,12 @@ module.exports = {
   async execute(interaction) {
     try {
       const embed2 = new EmbedBuilder()
-        .setImage('https://media.discordapp.net/attachments/1362182753537491054/1391907523342237706/Socials.gif?ex=68d9b565&is=68d863e5&hm=7da74d120c61db6a7928ff6e4c4d415c46e880508e44cd75fd2f32596ee45e83')
+        .setImage('https://media.discordapp.net/attachments/1362182753537491054/1391907523342237706/Socials.gif?ex=68dbafa5&is=68da5e25&hm=4671e4e7864f88ff6deab2cb132968430a82682d18e740cee3dd3ca804d8bb80')
         .setColor('#190e4e');
 
         const embed10 = new EmbedBuilder()
       .setDescription(`<:robuxbag:1421752726357938266> [Earn and Donate](https://www.roblox.com/games/100747960449802/Earn-and-Donate)\n
-<:robuxs:1421753470272147586> [PLS DONATE Z](https://www.roblox.com/games/129672063958972/PLS-DONATE-Z)\n
+<:robuxs:1421753470272147586> [PLS DONATE Z](https://www.roblox.com/games/76819241593867/PLS-DONATE-Z)\n
  <:taglineclub:1421754443128639509> [Tagline Club Roblox Group](https://www.roblox.com/communities/17015122/Tagline-Club#!/about)\n
 <:FakeNitroEmoji:1421753852331298836> [Twitter](https://x.com/txgline)\n
 <:youtube:1421777263887777822> [YouTube](https://www.youtube.com/@txglines)
